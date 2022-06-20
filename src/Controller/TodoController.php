1b24bcd6e2909adb9fa3b19cf2dcf5f6ef9b3704
Tomasz Strzelecki <strzelecki.tomasz@gmail.com>
2022-06-20 17:36:59 +0200

    find simple post endpoint to test frontend post method

--- src/Controller/TodoController.php
+++ src/Controller/TodoController.php
@@ -5,11 +5,12 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TodoController
 {
-    #[Route('/', name: 'index')]
+    #[Route('/', name: 'index', methods: ['GET'])]
     public function getResponse(): JsonResponse
     {
         return new JsonResponse([
@@ -18,5 +19,11 @@ class TodoController
             ['id' => 2, 'name' => 'Wash dishes'],
             ['id' => 4, 'name' => 'Do the laundry'],
         ]);
+    }
+
+    #[Route('/post', name: 'post', methods: ['POST'])]
+    public function postResponse(): Response
+    {
+        return new Response(''. Response::HTTP_OK);
     }
 }
